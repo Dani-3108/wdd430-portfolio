4c200c90c52cda8b062294b0d8e7a6eeed18fbbd
Dani-3108 <floresd477@gmail.com>
2026-09-26 15:36:47 -0600

    week3 Static and Dynamic Rendering

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,13 @@
+import { getProjects } from '@/lib/projects-db';
+
 export default async function ProjectPage() {
-  const res = await fetch('http://localhost:3000/api/projects');
-  const projects = await res.json();
+  const projects = await getProjects();
 
   return (
     <main className="container mx-auto px-4 py-12">
       <section className="text-center py-12">
         <h1 className="text-4xl font-bold mb-4">Projects Overview</h1>
+        <pre>{JSON.stringify(projects, null, 2)}</pre>
       </section>
     </main>
   );
